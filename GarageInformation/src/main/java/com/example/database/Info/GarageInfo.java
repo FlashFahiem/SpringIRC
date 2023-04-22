73a1dd7b7d3996ef82b19e1174fda6742fcbf2b2
Mohammed Fahiem A <fahiemrox@gmail.com>
2023-04-22 15:26:08 +0530

    Connection Between Spring and MySQL

---
+++ GarageInformation/src/main/java/com/example/database/Info/GarageInfo.java
@@ -0,0 +1,66 @@
+package com.example.database.Info;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "GInfo")
+public class GarageInfo {
+	
+	
+	@Id
+	@Column(name="GarageID")
+	private int garageId;
+	@Column(name="OwnerName")
+	private String ownerName;
+	@Column(name="OwnerNumber")
+	private int ownerNumber;
+	@Column(name="VehicleNumber")
+	private String vehicleNumber;
+	@Column(name="VehicleName")
+	private String vehicleName;
+	@Column(name="TotalVehicles")
+	private int totalVehicles;
+	
+	public int getGarageId() {
+		return garageId;
+	}
+	public void setGarageId(int garageId) {
+		this.garageId = garageId;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public int getOwnerNumber() {
+		return ownerNumber;
+	}
+	public void setOwnerNumber(int ownerNumber) {
+		this.ownerNumber = ownerNumber;
+	}
+	public String getVehicleName() {
+		return vehicleName;
+	}
+	public void setVehicleName(String vehicleName) {
+		this.vehicleName = vehicleName;
+	}
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+	public int getTotalVehicles() {
+		return totalVehicles;
+	}
+	public void setTotalVehicles(int totalVehicles) {
+		this.totalVehicles = totalVehicles;
+	}
+	
+	
+	
+}
